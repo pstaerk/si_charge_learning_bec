60020fcd9edc2e36dc3c720d10e4ca3313494ccb
Philip Loche <ploche@physik.fu-berlin.de>
2026-04-21 12:52:01 +0200

    Normalize spectra

--- figures/spectra.py
+++ figures/spectra.py
@@ -21,6 +21,15 @@ def wavenumber_to_thz(x):
     return x / _THZ_TO_WAVENUMBER
 
 
+def _trapezoid(y, x, xmin=None, xmax=None):
+    mask = np.ones(len(x), dtype=bool)
+    if xmin is not None:
+        mask &= x >= xmin
+    if xmax is not None:
+        mask &= x <= xmax
+    return np.trapezoid(y[mask], x[mask])
+
+
 @mpltex.acs_decorator
 def plot_scrum():
     fig, ax = plt.subplots(3, sharex=True, sharey=True)
@@ -28,9 +37,22 @@ def plot_scrum():
     fig.set_figheight(1.8 * fig.get_figheight())
     fig.set_figwidth(3.5)
 
-    labels = [r"Uncoupl.", r"Coupl. $\gamma$", r"Coupl. $\gamma_i$"]
-    for i, spectra in enumerate([bulk_uncoupled, bulk_global, bulk_local]):
+    ax[2].set_ylim(0, 1.75)
+
+    xmin = 20
+    xmax = 150
+    ax[2].set_xlim(xmin, xmax)
+
+    labels = [r"Uncoupl.", r"Coupl. $\gamma$", r"Coupl. $\gamma_i$", r"Uncoupl. fixed"]
+    bulk_spectra = [bulk_uncoupled, bulk_global, bulk_local, bulk_fixed]
+    sim_integrals = np.zeros(len(bulk_spectra))
+    for i, spectra in enumerate(bulk_spectra):
         ax[0].plot(spectra["nu"], spectra["susc_imag"], label=labels[i])
+        sim_integrals[i] = _trapezoid(spectra["susc_imag"], spectra["nu"], xmin, xmax)
+
+    exp_integral = _trapezoid(bulk_exp[:, 3], bulk_exp[:, 0], xmin, xmax)
+    exp_scale = np.mean(sim_integrals) / exp_integral
+    ax[0].plot(bulk_exp[:, 0], exp_scale * bulk_exp[:, 3], "k", ls="--", label="Experiment")
 
     for i, spectra in enumerate([book_uncoupled, book_global, book_local]):
         ax[1].plot(spectra["nu"], spectra["susc_imag"], label=labels[i])
@@ -38,10 +60,7 @@ def plot_scrum():
     for i, spectra in enumerate([cage_uncoupled, cage_global, cage_local]):
         ax[2].plot(spectra["nu"], spectra["susc_imag"], label=labels[i])
 
-    for i, spectra in enumerate([bulk_fixed]):
-        ax[0].plot(spectra["nu"], spectra["susc_imag"], label="Uncoupl. fixed")
 
-    ax[0].plot(bulk_exp[:, 0], bulk_exp[:, 3], "k", ls="--", label="Experiment")
     ax[-1].set_xlabel(r"$\omega$ [Thz]")
     secax = ax[0].secondary_xaxis(
         "top", functions=(thz_to_wavenumber, wavenumber_to_thz)
@@ -55,10 +74,7 @@ def plot_scrum():
     ax[0].tick_params(top=False, labeltop=False, bottom=False, labelbottom=False)
     ax[-1].tick_params(top=False, labeltop=False, bottom=True, labelbottom=True)
 
-    ax[0].legend(ncol=2)
-
-    ax[2].set_ylim(0, 1.75)
-    ax[2].set_xlim(20, 150)
+    ax[0].legend(loc="upper center", ncol=1, handlelength=1.5, bbox_to_anchor=(0.415, 1))
 
     add_subplotlabels(fig, ax, labels=["A", "B", "C"])
 
